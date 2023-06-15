bbaf246cfd2bd76032885bdb33f8a837efc5d261
devhabib <habibr152@gmail.com>
2023-06-15 23:48:10 +0600

    header completed with responsive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleHeader() {
+    var navbar = document.querySelector(".header__navbar");
+    var search = document.querySelector(".header__search");
+    navbar.classList.toggle("open");
+    search.classList.toggle("open");
+  }
+
+  var mobileToggle = document.querySelector(".header__mobile a");
+  mobileToggle.addEventListener("click", toggleHeader);
+});

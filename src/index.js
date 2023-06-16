9df4da5c83863e39ad2f5a4c96d3b5115770dcf4
devhabib <habibr152@gmail.com>
2023-06-16 15:12:59 +0600

    cleaned js file

--- src/index.js
+++ src/index.js
@@ -14,15 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
   mobileToggle.addEventListener("click", toggleHeader);
   const navLinks = document.querySelectorAll("nav ul li a");
 
-  // Add click event listeners to each menu item
   navLinks.forEach(function (link) {
     link.addEventListener("click", function (event) {
       event.preventDefault();
 
-      // Get the target section ID from the href attribute
       const targetSection = this.getAttribute("href");
 
-      // Scroll to the target section
       document.querySelector(targetSection).scrollIntoView({
         behavior: "smooth",
       });

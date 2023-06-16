23ff76fa14b67c6f3be74a8125865f9baeb94b7d
devhabib <habibr152@gmail.com>
2023-06-16 15:01:47 +0600

    added onepage custom scroller, added styles

--- src/index.js
+++ src/index.js
@@ -10,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var mobileToggle = document.querySelector(".header__mobile a");
   mobileToggle.addEventListener("click", toggleHeader);
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  // Add click event listeners to each menu item
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Get the target section ID from the href attribute
+      const targetSection = this.getAttribute("href");
+
+      // Scroll to the target section
+      document.querySelector(targetSection).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
 });
